ca679b643d9f836926aec7f2e39759c88ecf3dad
AiueoKashi <70052601+Aiueokashi@users.noreply.github.com>
2021-01-07 19:26:24 +0900

    Update clientConfig.js

--- structures/clientConfig.js
+++ structures/clientConfig.js
@@ -1,7 +1,6 @@
 
 module.exports = {
 	cooldown: 3000,//一度コマンドを使ってから同じコマンドが使えるまでの時間(ミリ秒)
-  allowMention: true,//接頭辞(!とか?)の代わりにbotをメンションしてもコマンドを起動するか(true|false)
-  handleEdits: true,//間違って送信したコマンドを編集して正しくした時にコマンドを起動するか(true|false)
-  
+	allowMention: true,//接頭辞(!とか?)の代わりにbotをメンションしてもコマンドを起動するか(true|false)
+	handleEdits: true,//間違って送信したコマンドを編集して正しくした時にコマンドを起動するか(true|false)
 };
